fada57478b763f37d2c15768e4d3ea8262fee33a
Igor Dragone <alu0101469652@ul.edu.es>
2024-04-08 17:21:48 +0000

    Changed Bot

--- bot.py
+++ bot.py
@@ -2,35 +2,37 @@ import pysmile
 import pysmile_license
 import random
 
-
-states = ["Atacar", "recoger_arma", "recoger_energ_a", "explorar", "huir",  "detectar_peligro" ]
+states = ["Atacar", "Recoger_Armas", "Recoger_Energia", "Explorar", "Huir",  "Detectar_Peligro" ]
 
 def botStateProbability(stValue = "Atacar" ):
     net = pysmile.Network()
     net.read_file("BotIaa.xdsl")
     st = stValue
-    h = "si"
-    w = "si"
-    ow = "si"
+    h = "Alta"
+    w = "armado"
+    ow = "armado"
     hn = "si"
     ne = "si"
     pw = "si"
     ph = "si"
-    evidence = { "Estado_Actual": st,  "H": h, "Copy_2_of_H": w, "Copy_5_of_H": ow, "Copy_of_H": hn, "Copy_3_of_H": ne, "Copy_4_of_H": pw, "Copy_6_of_H": ph }
+    evidence = { "St": st,  "H": h, "W": w, "OW": ow, "HN": hn, "NE": ne, "PW": pw, "PH": ph }
     for node_name, value in evidence.items():
         net.set_evidence(node_name, value)
     net.update_beliefs()
-    return net.get_node_value("Copy_of_Estado_Actual")
+    return net.get_node_value("st_1")
 
 def selectPosition(probabilities):
   indexes = list(range(len(probabilities)))
-  return random.choices(indexes, weights=probabilities, k=1)[0]
+  new_index = random.choices(indexes, weights=probabilities, k=1)[0]
+  return new_index
 
-if __name__ == "__main__":
+def main():
     print("Bot's probability at t+1 is:")
     print(states)
     prob = botStateProbability()
     newState = selectPosition(prob)
     for i in range(10):
         prob = botStateProbability(states[newState])
+        print(prob)
         newState = selectPosition(prob)
+main()
